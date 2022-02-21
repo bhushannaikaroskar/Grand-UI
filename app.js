54ccd70e2e7591184bc455ff51aa303b0c598377
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-02-21 19:03:23 +0530

    refactor: used switch case for links

--- app.js
+++ app.js
@@ -23,28 +23,41 @@ arrayOfListItems.forEach((element)=>{
 const arr = window.location.href.split("#");
 const value = arr[1];
 
-if(value === "avatar"){
-    activeTab(componentList,arrayOfListItems[1]);
-}else if(value === "installation"){
-    activeTab(componentList,arrayOfListItems[0]);
-}else if(value === "alert"){
-    activeTab(componentList,arrayOfListItems[2]);
-}else if(value === "badge"){
-    activeTab(componentList,arrayOfListItems[3]);
-}else if(value === "button"){
-    activeTab(componentList,arrayOfListItems[4]);
-}else if(value === "cards"){
-    activeTab(componentList,arrayOfListItems[5]);
-}else if(value === "image"){
-    activeTab(componentList,arrayOfListItems[6]);
-}else if(value === "input"){
-    activeTab(componentList,arrayOfListItems[7]);
-}else if(value === "text-utilities"){
-    activeTab(componentList,arrayOfListItems[8]);
-}else if(value === "lists"){
-    activeTab(componentList,arrayOfListItems[9]);
-}else if(value === "navbar"){
-    activeTab(componentList,arrayOfListItems[10]);
-}else{
-    activeTab(componentList,arrayOfListItems[1]);
-}
+switch(value) {
+    case "avatar":
+        activeTab(componentList,arrayOfListItems[1])
+        break;
+    case "installation":
+        activeTab(componentList,arrayOfListItems[0])
+        break;
+    case "alert":
+        activeTab(componentList,arrayOfListItems[2])
+        break;
+    case "badge":
+        activeTab(componentList,arrayOfListItems[3])
+        break;
+    case "button":
+        activeTab(componentList,arrayOfListItems[4])
+        break;
+    case "cards":
+        activeTab(componentList,arrayOfListItems[5])
+        break;
+    case "image":
+        activeTab(componentList,arrayOfListItems[6])
+        break;
+    case "input":
+        activeTab(componentList,arrayOfListItems[7])
+        break;
+    case "text-utilities":
+        activeTab(componentList,arrayOfListItems[8])
+        break;
+    case "lists":
+        activeTab(componentList,arrayOfListItems[9])
+        break;
+    case "navbar":
+        activeTab(componentList,arrayOfListItems[10])
+        break;
+    default:
+        activeTab(componentList,arrayOfListItems[1]);
+        break;
+  }
