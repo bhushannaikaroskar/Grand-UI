4b146e96b4bec9de566d2afa1c9a1fbbc49d8d10
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-02-22 00:11:29 +0530

    add: some minor changes

--- app.js
+++ app.js
@@ -73,3 +73,33 @@ switch(value) {
         activeTab(componentList,arrayOfListItems[1]);
         break;
   }
+
+//   DARK MODE LOGIC
+
+let theme = localStorage.getItem("data-theme");
+const toggleThemeButton = document.querySelector("#toggle-theme");
+
+const toggleTheme = ()=>{
+    theme = localStorage.getItem("data-theme")
+    if(theme === "dark"){
+        document.documentElement.setAttribute("data-theme","light")
+        localStorage.setItem("data-theme","light")
+        toggleThemeButton.querySelector("span").innerHTML = "dark_mode"
+    }else{
+        document.documentElement.setAttribute("data-theme","dark")
+        localStorage.setItem("data-theme","dark")
+        toggleThemeButton.querySelector("span").innerHTML = "light_mode"
+
+    }
+}
+
+if(theme === "dark"){
+    document.documentElement.setAttribute("data-theme","dark")
+    toggleThemeButton.querySelector("span").innerHTML = "light_mode"
+}else{
+    document.documentElement.setAttribute("data-theme","light")
+    toggleThemeButton.querySelector("span").innerHTML = "dark_mode"
+
+}
+
+toggleThemeButton.addEventListener("click",toggleTheme)
