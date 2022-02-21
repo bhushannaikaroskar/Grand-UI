f0fd284ce6be6c37f433ccaed000aea5d50a5843
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-02-22 00:33:23 +0530

    fix: some css bugs

---
+++ landing-page.js
@@ -0,0 +1,29 @@
+
+let theme = localStorage.getItem("data-theme");
+const toggleThemeButton = document.querySelector("#toggle-mode");
+console.log(toggleThemeButton)
+
+const toggleTheme = ()=>{
+    theme = localStorage.getItem("data-theme")
+    if(theme === "dark"){
+        document.documentElement.setAttribute("data-theme","light")
+        localStorage.setItem("data-theme","light")
+        toggleThemeButton.querySelector("span").innerHTML = "dark_mode"
+    }else{
+        document.documentElement.setAttribute("data-theme","dark")
+        localStorage.setItem("data-theme","dark")
+        toggleThemeButton.querySelector("span").innerHTML = "light_mode"
+
+    }
+}
+
+if(theme === "dark"){
+    document.documentElement.setAttribute("data-theme","dark")
+    toggleThemeButton.querySelector("span").innerHTML = "light_mode"
+}else{
+    document.documentElement.setAttribute("data-theme","light")
+    toggleThemeButton.querySelector("span").innerHTML = "dark_mode"
+
+}
+
+toggleThemeButton.addEventListener("click",toggleTheme)
