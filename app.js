86b388f4eddf2c5f9b23c74c5a6037e845233d83
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-02-23 01:34:45 +0530

    add: white logo for dark theme

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ const value = arr[1];
         activeTab(componentList,objectOfListItems["slider"])
         break;
     default:
-        activeTab(componentList,arrayOfListItems[1]);
+        activeTab(componentList,objectOfListItems["avatar"]);
         break;
   }
 
@@ -86,6 +86,7 @@ const value = arr[1];
 
 let theme = localStorage.getItem("data-theme");
 const toggleThemeButton = document.querySelector("#toggle-theme");
+const logo = document.querySelector(".nav-logo img");
 
 const toggleTheme = ()=>{
     theme = localStorage.getItem("data-theme")
@@ -93,21 +94,23 @@ const toggleTheme = ()=>{
         document.documentElement.setAttribute("data-theme","light")
         localStorage.setItem("data-theme","light")
         toggleThemeButton.querySelector("span").innerHTML = "dark_mode"
+        logo.src = "/assets/landing/grand-logo.png"
     }else{
         document.documentElement.setAttribute("data-theme","dark")
         localStorage.setItem("data-theme","dark")
         toggleThemeButton.querySelector("span").innerHTML = "light_mode"
-
+        logo.src = "/assets/landing/grand-logo-dark.png"
     }
 }
 
 if(theme === "dark"){
     document.documentElement.setAttribute("data-theme","dark")
     toggleThemeButton.querySelector("span").innerHTML = "light_mode"
+    logo.src = "/assets/landing/grand-logo-dark.png"
 }else{
     document.documentElement.setAttribute("data-theme","light")
     toggleThemeButton.querySelector("span").innerHTML = "dark_mode"
-
+    logo.src = "/assets/landing/grand-logo.png"
 }
 
 toggleThemeButton.addEventListener("click",toggleTheme)
